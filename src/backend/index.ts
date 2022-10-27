ba5bfd3ee8c53a4e06f030812ed2ab1ae1e2aebc
Mike Valeriano <mkvlrn@gmail.com>
2022-10-27 17:24:00 -0300

    chore: fix backend build step

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { config as dotenvConfig } from 'dotenv';
 import cron from 'node-cron';
+import { join } from 'path';
 import 'reflect-metadata';
 import { container } from 'tsyringe';
 import { Worker } from 'worker_threads';
@@ -14,7 +15,7 @@ container.register(PrismaClient, { useValue: new PrismaClient() });
 const server = container.resolve(Server);
 
 function doScraping() {
-  const worker = new Worker('./worker/scraping.js');
+  const worker = new Worker(join(__dirname, './worker/scraping.js'));
   worker.on('message', (data: string) => {
     if (data.includes('FAILED')) Logger.error(data);
     else Logger.info(data);
